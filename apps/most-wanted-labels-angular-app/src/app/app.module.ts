56590df6a5c29971ae9c29760c0a0081b48f50b3
Patrick Blinkhorn <contact.blinkhorn@gmail.com>
2021-12-31 19:52:09 -0500

    basic header and general layout implemented

--- apps/most-wanted-labels-angular-app/src/app/app.module.ts
+++ apps/most-wanted-labels-angular-app/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
